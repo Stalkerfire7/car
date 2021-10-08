b9a895cd1a881d4995a52e41d6d2054b9740b294
Stalkerfire7 <timofrank96@t-online.de>
2021-10-08 15:56:20 +0200

    init laravel

---
+++ laravel/resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.app')
+
+@section('f')
+    <div class="m-auto w-4/5 py-24">
+        <div class="text-center">
+        <h1 class="text-5xl uppercase font-bold">Cars</h1>
+        </div>
+
+    </div>
+@endsection
---
+++ laravel/resources/views/layout/app.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link href="../../css/app.css">
+</head>
+<body class="bg-gradient-to-r from-gray-100 to-gray-200">
+@yield('f')
+</body>
+</html>
